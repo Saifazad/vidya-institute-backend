127fb2d8e19700e10f78fd48221e97458efb5447
Saif Ali <saifali.dev01@gmail.com>
2025-12-31 20:34:04 +0530

    fixed database connection with ssl and port

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 17821, // Aiven ka port zaroori hai
+  ssl: {
+    rejectUnauthorized: false, // Aiven ke liye ye line sabse zaroori hai!
+  },
 });
-
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL Connection Failed", err);
